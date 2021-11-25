75cc3e3690ba5198a2479087313c60fccceba1f0
Zachary <vurtnec@users.noreply.github.com>
2021-11-25 17:14:25 +0800

    pub sub poc

---
+++ OpenTelemetry.js
@@ -0,0 +1,18 @@
+/* app.js */
+
+const express = require("express");
+const { countAllRequests } = require("./monitoring");
+
+
+const PORT = process.env.PORT || "8081";
+const app = express();
+app.use(countAllRequests());
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(parseInt(PORT, 10), () => {
+    console.log(`Listening for requests on http://localhost:${PORT}`);
+});
+
