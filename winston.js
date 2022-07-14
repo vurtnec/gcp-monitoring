f7f2539dc819a793dd5c2b49558dce92ee3904ae
Zachary <vurtnec@users.noreply.github.com>
2022-07-14 17:40:21 +0800

    test9

--- winston.js
+++ winston.js
@@ -20,5 +20,5 @@ const logger = winston.createLogger({
 });
 
 // Writes some log entries
-logger.error('warp nacelles offline main1');
-logger.info('shields at main1%');
+logger.error('warp nacelles offline test9');
+logger.info('shields at test9%');
